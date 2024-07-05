a2ade30edaee2eb21acc098d16e8e1f5c653460a
dharmendra-kumarYadav <140683752+dharmendra-kumarYadav@users.noreply.github.com>
2024-07-05 23:26:30 +0530

    first commit

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import { FaSquareInstagram } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -9,31 +9,48 @@ import { SiGmail } from "react-icons/si";
 const Contact = () => {
   return (
     <>
-      <div className='container contact' id='contact'>
+      <div className="container contact" id="contact">
         <h1>CONTACT ME</h1>
-        <div className='contact-icon'
+        <div
+          className="contact-icon"
           data-aos="zoom-in-up"
           data-aos-duration="1000"
         >
-          <a href='https://www.instagram.com/dharmendra_kumar_yadav25/' target='_blank' className='items'>
-            <FaSquareInstagram className='icons'/>
+          <a
+            href="https://www.instagram.com/dharmendra_kumar_yadav25/"
+            target="_blank"
+            className="items"
+          >
+            <FaSquareInstagram className="icons" />
           </a>
-          <a href='https://www.facebook.com/profile.php?id=100035191360093' target='_blank' className='items'>
-            <FaFacebook className='icons'/>
+          <a
+            href="https://www.facebook.com/profile.php?id=100035191360093"
+            target="_blank"
+            className="items"
+          >
+            <FaFacebook className="icons" />
           </a>
-          <a href='https://www.linkedin.com/in/dharmendra-kumar-yadav-8a8126256/' target='_blank' className='items'>
-            <FaLinkedin className='icons'/>
+          <a
+            href="https://www.linkedin.com/in/dharmendra-kumar-yadav-8a8126256/"
+            target="_blank"
+            className="items"
+          >
+            <FaLinkedin className="icons" />
           </a>
-          <a href='https://github.com/dharmendra-kumarYadav' target='_blank' className='items'>
-            <FaGithubSquare className='icons'/>
+          <a
+            href="https://github.com/dharmendra-kumarYadav"
+            target="_blank"
+            className="items"
+          >
+            <FaGithubSquare className="icons" />
           </a>
-          <a href='mailto:ydharmendrakumar878@gmail.com' target='_blank' className='items'>
-           <SiGmail className='icons'/>
+          <a href="mailto:ydharmendrakumar878@gmail.com" className="items">
+            <SiGmail className="icons" />
           </a>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
